7c127f5cc616e3f26ca70c4860060fe5f0a209ac
lesejong <umlstudy@gmail.com>
2023-02-21 17:51:47 +0900

    4.1 tailwind..

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
-import { Inter } from '@next/font/google'
+import type { NextPage } from "next";
 
-const inter = Inter({ subsets: ['latin'] })
+const Home:NextPage =()=>{
+  return <div className="bg-slate-400 py-10 px-5 grid gap-10">
+    <div className="bg-white p-6 rounded-2xl shadow-xl">
+      <span className="font-semibold text-3xl">Select Item</span>
+      <div className="flex justify-between my-2">
+        <span className="text-gray-500">Gray Chair</span>
+        <span className="font-semibold">$19</span>
+      </div>
+      <div className="flex justify-between">
+        <span className="text-gray-500">Tooly Table</span>
+        <span className="font-semibold">$81</span>
+      </div>
+      <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+        <span>Total</span>
+        <span className="font-semibold">$10</span>
+      </div>
+      <div className="mt-5 p-2 bg-blue-500 text-white text-center rounded-xl w-2/4 mx-auto">Checkout</div>
+    </div>
+    <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+    <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+    <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
 
-export default function Home() {
-  return null;
+  </div>;
 }
+
+export default Home;
